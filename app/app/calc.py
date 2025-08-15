eecbe9c688bdb2b771186f1b2784c4fda3be9bfd
Anoob <anoob.vm@mavapartners.com>
2025-08-15 11:46:11 +0530

    updates till db configurations

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+def add(x: float, y: float) -> float:
+    """
+    Add two numbers.
+
+    :param x: First number
+    :param y: Second number
+    :return: Sum of x and y
+    """
+    return x + y
